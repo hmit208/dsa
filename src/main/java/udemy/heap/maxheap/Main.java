9b930e13d5a1dfb8f9367d80c69dba3e58e4d85a
ThanhHoang <thanhhoang@beeketing.net>
2022-02-06 00:03:02 +0700

    heap

---
+++ src/main/java/udemy/heap/maxheap/Main.java
@@ -0,0 +1,25 @@
+package udemy.heap.maxheap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Heap heap = new Heap(10);
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+        System.out.println(heap.peek());
+
+//        heap.delete(1);
+//        heap.printHeap();
+
+        heap.sort();
+        heap.printHeap();
+    }
+}
---
+++ src/main/java/udemy/heap/priorityqueue/Main.java
@@ -0,0 +1,23 @@
+package udemy.heap.priorityqueue;
+
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        pq.add(25);
+        pq.add(-22);
+        pq.add(1343);
+        pq.add(54);
+        pq.add(0);
+        pq.add(-3492);
+        pq.add(429);
+
+        System.out.println(pq.peek());
+        System.out.println(pq.remove());
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+    }
+}
